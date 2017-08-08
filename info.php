3cac8397e701dd341aad94553a6602f891b5e600
labby <rp@cms-lab.com>
2017-08-08 12:55:13 +0200

    initial release

---
+++ info.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *  @module         SimplePageHead
+ *  @version        see info.php of this module
+ *  @authors        Chio, Thorn, FreeSbee
+ *  @copyright      2006-2011 Chio
+ *  @license        GNU General Public License
+ *  @license terms  see info.php of this module
+ *  @platform       see info.php of this module
+ *  @requirements   PHP 5.2.x and higher
+ */
+
+$module_directory     = 'simplepagehead';
+$module_name          = 'SimplePageHead';
+$module_function      = 'snippet';
+$module_version       = '0.51';
+$module_platform      = '1.x';
+$module_author        = 'Chio, with a litte help from thorn. Extended for use with other modules by Christoph Marti.';
+$module_license       = 'GNU General Public License';
+$module_terms         = 'none';
+$module_description   = 'Snippet to generate head tags (title, keywords.. ) better, simpler and also on newspages, bakery and other modules. Function can be invoked from the template. Simplest call: simplepagehead();';
+$module_guid          = 'b29996e9-6c1a-409a-a698-ccb90ef97793';
+?>
